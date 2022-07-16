4d3a414de952186e92d818b3e17cd9107dd8dc05
HerbertRHolder <herbrholder@gmail.com>
2022-07-15 20:30:27 -0500

    fixing issue with getNode()

--- main.cpp
+++ main.cpp
@@ -25,8 +25,10 @@ public: //for debugging keep public
 
   Node<t> *firstNode() const;
   Node<t> *lastNode() const;
-  Node<t> *findNode(int pos) const;
-  Node<t> *findItem(t val) const;
+  Node<t> *findNode(t val) const;
+  Node<t> *getNode(int pos) const;
+  t getValue(int pos) const;
+  void setvalue(int pos, t val);
   void pop();
   void destroyNode(t &item);
   void destroylist();
@@ -56,6 +58,59 @@ Dlinklist <t>::Dlinklist(t val){
     tail = head;
     count++;
 }
+
+template <class t>
+Dlinklist <t>::Node<t> *firstNode() { return head; }
+template <class t>
+Dlinklist <t>::Node<t> *lastNode() { return tail; }
+
+template <class t>
+Dlinklist <t>::Node<t> *findNode(t val) {
+    Node<t> *tmp = head;
+    while (tmp != NULL) {
+      if (tmp->value == val) {
+        return tmp;
+      }
+      tmp = tmp->next;
+    }
+    return NULL;
+  } // end of find node
+
+template <class t>
+  Dlinklist <t>::Node<t> *getNode(int pos) {
+    if (pos < 0 || head == NULL) {
+      return NULL;
+    }
+    int i = 0;
+    Node<t> *tmp = head;
+    while (tmp->next != NULL && i < pos) {
+      tmp = tmp->next;
+      i++;
+    }
+    return tmp;
+  } // end of getNode
+
+  template <class t>
+   Dlinklist <t>::t getValue(int pos) {
+    Node<t> *node = getNode(pos);
+    if (node != NULL) {
+      return node->value;
+    } else {
+      cout << "Value does not exist" << endl;
+    }
+
+  } // end getvalue
+
+  // Sets the value given the position
+  template <class t>
+    void setvalue(int pos, t val) {
+    Node<t> *node = getNode(pos);
+    if (node != NULL) {
+      node->value = val;
+    } else {
+      cout << "position does not exist" << endl;
+    }
+  } // end of setvalue()
 
 int main() {
   
